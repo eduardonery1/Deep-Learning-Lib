4f1184fbfcda447bab3635425aa44072685f2d5c
eduardonery1 <48776305+eduardonery1@users.noreply.github.com>
2020-11-26 07:13:06 -0300

    Add files via upload

---
+++ dl/loss.py
@@ -0,0 +1,26 @@
+"""
+A loss function measures how good our predictions are,
+we can use this to adjust the parameters of our network
+"""
+import numpy as np
+
+from dl.tensor import Tensor
+
+class Loss:
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        raise NotImplementedError
+
+
+class MSE(Loss):
+    """
+    MSE is mean squared error, although we're
+    just going to do total squared error
+    """
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        return np.sum((predicted - actual) ** 2)
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        return 2 * (predicted - actual)
---
+++ dl/train.py
@@ -0,0 +1,27 @@
+"""
+Here's a function that can train a neural net
+"""
+
+from dl.tensor import Tensor
+from dl.nn import NeuralNet
+from dl.loss import Loss, MSE
+from dl.optim import Optimizer, SGD
+from dl.data import DataIterator, BatchIterator
+
+
+def train(net: NeuralNet,
+          inputs: Tensor,
+          targets: Tensor,
+          num_epochs: int = 5000,
+          iterator: DataIterator = BatchIterator(),
+          loss: Loss = MSE(),
+          optimizer: Optimizer = SGD()) -> None:
+    for epoch in range(num_epochs):
+        epoch_loss = 0.0
+        for batch in iterator(inputs, targets):
+            predicted = net.forward(batch.inputs)
+            epoch_loss += loss.loss(predicted, batch.targets)
+            grad = loss.grad(predicted, batch.targets)
+            net.backward(grad)
+            optimizer.step(net)
+        print(epoch, epoch_loss)
